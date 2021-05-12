9721543ab5afdaafda47d1d1c8c6d71b73a98abf
Abel <abeldewit@gmail.com>
2021-05-12 18:10:44 +0200

    D.I.S.P.L.A.C.E.M.E.N.T
    - Added displacement plots
    
    TODO:
    - Plot displacements
    - Read out displacement on certain points of the skin (the sensor points)

--- Simulation/Simulator/FEM/AbFEM.py
+++ Simulation/Simulator/FEM/AbFEM.py
@@ -10,8 +10,8 @@ class SkinModel:
     def __init__(self):
         self.fem = FEModel3D()
         self.t = 1
-        self.E = 3824
-        self.nu = 0.3
+        self.E = 0.001
+        self.nu = 0.17
 
         self.width = None
         self.height = None
@@ -22,6 +22,8 @@ class SkinModel:
 
         self.node_name_list = []
         self.node_matrix = None
+        self.plate_matrix = None
+        self.factors = None
 
     def create_nodes(self, width, height, layers=1, mesh_size=1):
 
@@ -36,7 +38,10 @@ class SkinModel:
         y_lsp = np.linspace(0, height, y_steps)
         self.ylsp = y_lsp
 
-        self.node_matrix = np.array([[["", ""] for _ in range(len(self.xlsp))] for _ in range(len(self.ylsp))], dtype=object)
+        layer_vec = ["" for _ in range(self.layers)]
+        x_mat = [layer_vec for _ in range(len(self.xlsp))]
+        self.node_matrix = np.array([x_mat for _ in range(len(self.ylsp))], dtype=object)
+
         print(self.node_matrix.shape)
         for l in range(layers):
             for x, y in itertools.product(range(len(x_lsp)), range(len(y_lsp))):
@@ -53,7 +58,10 @@ class SkinModel:
         if (self.width is None) | (self.height is None) | (self.layers is None):
             raise ValueError("Tried to add plates without nodes")
 
-        for l in range(self.layers-1):
+        shap = self.node_matrix.shape
+        self.plate_matrix = np.array([[["" for _ in range(shap[2])] for _ in range(shap[1]-1)] for _ in range(shap[0]-1)], dtype=object)
+        len_layer = self.layers - 1 if self.layers > 1 else 1
+        for l in range(len_layer):
             for i, j in itertools.product(range(len(self.xlsp) - 1), range(len(self.ylsp) - 1)):
 
                 n1 = 'N' + str(int(i)) + '.' + str(int(j)) + '.' + str(int(l))
@@ -65,25 +73,53 @@ class SkinModel:
 
                 self.fem.AddPlate(name, n1, n2, n4, n3, self.t, self.E, self.nu)
 
+                self.plate_matrix[i, j, l] = name
+
     #TODO Look into padding the nodes of the FEM
 
     def get_node_mat(self):
         return self.node_matrix
 
-    def input_to_load(self, inp, max_force):
+    def input_to_load(self, img, max_force):
         # For now, 100 by 100 input, 10 by 10 nodes -> window = -5 to +5
         # So, for index i the window would be from i*10 to i*10+10
 
-        window = self.mesh_size
-        for x, y in itertools.product(range(len(self.node_matrix)), range(len(self.node_matrix[0]))):
-            node_filter = inp[
-                          x*window:x*window+window,
-                          y*window:y*window+window
-                          ]
-            avg = np.mean(node_filter)
+        x_step = self.xlsp[1]
+        y_step = self.ylsp[1]
+
+        s = self.plate_matrix.shape
+
+        for x, y in itertools.product(range(s[0]), range(s[1])):
+            x_0 = int(x*x_step)
+            x_1 = int(x*x_step + y_step)
+            y_0 = int(y*y_step)
+            y_1 = int(y*y_step + y_step)
+
+            window = img[x_0:x_1, y_0:y_1]
+            avg = np.mean(window)
+
             if avg != 0.0:
                 force = (avg / 255) * max_force
-                self.fem.AddNodeLoad(self.node_matrix[x, y, 0], "FZ", -force)
+
+                plat_name = self.plate_matrix[x, y, 0]
+
+                case_name = 'Case 1' #+plat_name[1:]
+                self.fem.Plates[plat_name].pressures.append([force, case_name])
+
+
+
+
+        # for x, y in itertools.product(range(len(self.node_matrix)), range(len(self.node_matrix[0]))):
+        #     node_filter = inp[
+        #                   x*window:x*window+window,
+        #                   y*window:y*window+window
+        #                   ]
+        #     avg = np.mean(node_filter)
+        #     if avg != 0.0:
+        #         force = (avg / 255) * max_force
+                #self.fem.AddNodeLoad(self.node_matrix[x, y, 0], "FZ", -force)
+
+
 
     def __define_support(self, type="Pinned"):
         for l in range(self.layers):
@@ -100,38 +136,38 @@ class SkinModel:
                 elif type == "Fixed":
                     node.SupportDX, node.SupportDY, node.SupportDZ, node.SupportRX, node.SupportRY, node.SupportRZ = True, True, True, True, True, True
 
-
     def get_model(self):
         return self.fem
 
     def analyse(self):
-        self.__define_support()
+        self.__define_support(type='Fixed')
 
         self.fem.Analyze(check_statics=True, sparse=True)
 
     def visualise(self):
         Visualization.RenderModel(self.fem, text_height=0.5, deformed_shape=False, deformed_scale=30,
-                                  render_loads=True, color_map='Mx', combo_name='Combo 1', case=None)
-
-
+                                  render_loads=True, color_map='dz', case=None, combo_name='Combo 1')
 
 
 if __name__ == '__main__':
     skin_model = SkinModel()
-    skin_model.create_nodes(100, 100, layers=2, mesh_size=5)
+    skin_model.create_nodes(100, 100, layers=1, mesh_size=5)
     skin_model.create_plates()
 
     # TODO normalize dsize to width & height (??)
 
     # TODO create method to read in (sequential) images
     inp = cv2.resize(
-        cv2.imread('../input/circle blur.png', cv2.IMREAD_GRAYSCALE),
+        cv2.imread('../input/hand.png', cv2.IMREAD_GRAYSCALE),
         dsize=(100, 100),
         interpolation=cv2.INTER_CUBIC
     )
 
-    skin_model.input_to_load(inp, 1000)
+    skin_model.input_to_load(inp, 10)
     skin_model.analyse()
+
+    print()
+
     skin_model.visualise()
 
     sys.exit()
